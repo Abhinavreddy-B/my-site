29f17b718751c5b7f15fe37e271710084ad98651
abhinav <abhinavreddyboddu@gmail.com>
2022-03-15 12:37:59 +0530

    v3 own-carousel

--- data.js
+++ data.js
@@ -16,62 +16,13 @@ let data;
     ],
   };
 
-  const skills = [
-    { name: 'Javascript', level: 85 },
-    { name: 'CSS/SASS', level: 85 },
-    { name: 'React JS', level: 80 },
-    { name: 'React Native', level: 75 },
-    { name: 'ML / AI', level: 60 },
-    { name: 'Django', level: 80 },
-    { name: 'Express.js', level: 80 },
-    { name: 'C/C++', level: 85 },
-    { name: 'MySQL', level: 60 },
-    { name: 'Shell Scripting', level: 60 },
-    { name: 'DS & Algorithms', level: 80 },
-    { name: 'OOPs', level: 70 },
-    { name: 'DBMS', level: 70 },
-    { name: 'Operating Systems', level: 50 },
-  ];
+  const images = {
+    sources: [
+      "https://i.pinimg.com/originals/e4/3b/30/e43b30464e7f8dfd8af62ab84909f436.png",
+      "https://www.quotemaster.org/images/4b/4bc43b36662d337adabea77e2d68f165.png",
+      "https://i.redd.it/y3bg31c45kj01.jpg"
+    ]
+  }
 
-  const projects = [
-    {
-      name: 'Classity',
-      image: './img/Classity.png',
-      about:
-        'Platform to allow instructors from all over the world to create and teach courses. Students can enrol those courses and learn through them',
-      code: 'https://github.com/Domino-Developers/Classity',
-      view: 'https://classity-v09.herokuapp.com/',
-    },
-    {
-      name: 'Ad Agency',
-      image: './img/AdAgency.png',
-      about:
-        'Management system for an Ad Agency to store and manipulate Actors, Brands, Directors, add contracts between them and much more with a CLI in Python',
-      code: 'https://github.com/tichnas/AdAgency',
-    },
-    {
-      name: 'IIIT-GPS',
-      image: './img/IIIT-GPS.png',
-      about:
-        'Platform to help students better understand and prepare for the courses they will be taking in upcoming semesters. Look at reviews, learning curve, etc.',
-      code: 'https://github.com/tichnas/IIIT-GPS',
-      view: 'https://tichnas.github.io/IIIT-GPS',
-    },
-    {
-      name: 'C Shell',
-      image: './img/C Shell.png',
-      about:
-        'Bash-like command interpreter, written in C, with support for process management, piping/redirection and built-in as well as system commands',
-      code: 'https://github.com/tichnas/C-Shell',
-    },
-    {
-      name: 'Advanced xv6',
-      image: './img/xv6.png',
-      about:
-        'Improved xv6 with new scheduling techniques, system calls and user programs. Analysis of different scheduling algorithms is done using Python',
-      code: 'https://github.com/tichnas/Advanced-xv6',
-    },
-  ];
-
-  data = { about, skills, projects };
+  data = { about,images };
 })();
--- script.js
+++ script.js
@@ -9,20 +9,27 @@ const typeStart = () => {
     if (sentence === sentences.length) return type(0, letter, erasing);
 
     if (letter === -1) {
-      setTimeout(() => type(sentence + 1, 0, false), letterWait);
+      var cur = document.querySelector('.cursor')
+      cur.innerText = '';
+      setTimeout(() => type(sentence + 1, 0, false), sentenceWait / 2);
     } else if (letter === sentences[sentence].length) {
+      var cur = document.querySelector('.cursor')
+      cur.innerText = '';
       setTimeout(() => type(sentence, letter - 1, true), sentenceWait);
     } else if (erasing) {
+      var cur = document.querySelector('.cursor')
+      cur.innerText = '|';
       typingElement.innerText = typingElement.innerText.slice(0, letter);
-
       setTimeout(() => type(sentence, letter - 1, true), letterWait);
     } else {
-      if(sentences[sentence][letter]=='.'){
-        var temp = ' ' + sentences[sentence][letter+1];
+      var cur = document.querySelector('.cursor')
+      cur.innerText = '_';
+      if (sentences[sentence][letter] == '.') {
+        var temp = ' ' + sentences[sentence][letter + 1];
         typingElement.innerText += temp;
         setTimeout(() => type(sentence, letter + 2, false), letterWait);
       }
-      else{
+      else {
         typingElement.innerText += sentences[sentence][letter];
         setTimeout(() => type(sentence, letter + 1, false), letterWait);
       }
@@ -32,3 +39,56 @@ const typeStart = () => {
 };
 
 typeStart();
+
+/*const carl = (){
+    if()
+}*/
+
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+var contin = true;
+async function carlstart(pos = 0) {
+  
+  const timeint = 1000;
+  const img = document.getElementsByClassName('image')[0];
+  const lst = document.getElementsByClassName('list');
+  const n = 3;
+  while (contin) {
+    img.style.opacity = 0;
+    
+    lst[pos].style.width = '20%';
+    lst[(pos+1) % 3].style.width = '10%';
+    lst[(pos + 2) % 3].style.width = '10%';
+    setTimeout(function () {
+      img.src = data.images.sources[pos % 3];
+      img.style.opacity = 100;
+      pos++;
+      pos = pos % 3;
+    }, 500);
+    await timer(5000);
+  }
+  
+  //setTimeout(carlstart(pos+1),5000);
+}
+
+const slideto = (pos) => {
+  var temp = pos;
+  
+  contin = false;
+  setTimeout(function(){
+    contin=true;
+    carlstart(pos%3);
+  },5000);
+  var img = document.getElementsByClassName('image')[0];
+  img.style.opacity = 0;
+  const lst = document.getElementsByClassName('list');
+  setTimeout(function () {
+    img.src = data.images.sources[pos - 1];
+    img.style.opacity = 100;
+  }, 500);
+  lst[pos - 1].style.width = '20%';
+  lst[pos % 3].style.width = '10%';
+  lst[(pos + 1) % 3].style.width = '10%';
+}
+
+slideto(1);
